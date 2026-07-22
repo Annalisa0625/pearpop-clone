de528249ebd87005812c8098c5bd22303e91d670
Annalisa0625 <giobanni.stoer@gmail.com>
2026-07-22 22:58:54 +0900

    refactor: remove temporary Link inquiry shortcut

--- app/creator/link/layout.tsx
+++ app/creator/link/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import CreatorLinkInboxShortcut from "./_components/CreatorLinkInboxShortcut";
-
 export default function CreatorLinkLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <CreatorLinkInboxShortcut />
-    </>
-  );
+  return children;
 }
