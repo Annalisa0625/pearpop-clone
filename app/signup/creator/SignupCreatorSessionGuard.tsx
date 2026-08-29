63cef505b147d631ac313f46b656c334986f3812
Annalisa0625 <giobanni.stoer@gmail.com>
2026-08-30 01:52:49 +0900

    fix: enable creator price picker auto-close

--- app/signup/creator/SignupCreatorSessionGuard.tsx
+++ app/signup/creator/SignupCreatorSessionGuard.tsx
@@ -4,6 +4,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { createSupabaseBrowserClient } from "@/lib/supabase/browser";
+import CreatorPricePickerCollapseBehavior from "./CreatorPricePickerCollapseBehavior";
 import SignupCreatorClient from "./SignupCreatorClient";
 
 const SIGNUP_DRAFT_KEY = "trendre_creator_signup_draft_v8_compact";
@@ -127,5 +128,10 @@ export default function SignupCreatorSessionGuard({
     );
   }
 
-  return <SignupCreatorClient isCreatorOnly={isCreatorOnly} />;
+  return (
+    <>
+      <CreatorPricePickerCollapseBehavior />
+      <SignupCreatorClient isCreatorOnly={isCreatorOnly} />
+    </>
+  );
 }
